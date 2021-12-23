03c236f7282f75bdeeac2a2b9e06767800f0d3d3
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2021-12-23 19:24:44 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {increment, decrement} from "./actions";
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ? <h3>Valuable Information</h3> : ""}
+    </div>
+  );
+}
+
+export default App;
